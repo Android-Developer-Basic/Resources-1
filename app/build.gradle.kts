c346c6b2ef63b5dd3200a38b84758280e4482c31
Nikolay Kochetkov <motorro@gmail.com>
2023-11-14 16:58:52 +0100

    Special formatting

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "otus.gpb.appresources"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
--- app/src/main/java/otus/gpb/appresources/MainActivity.kt
+++ app/src/main/java/otus/gpb/appresources/MainActivity.kt
@@ -3,11 +3,14 @@ package otus.gpb.appresources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import java.time.LocalDate
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var catsInHouse: TextView
     private lateinit var animalName: TextView
+    private lateinit var weightOfCat: TextView
+    private lateinit var bdOfCat: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +21,12 @@ class MainActivity : AppCompatActivity() {
 
         animalName = findViewById(R.id.animal_name)
         setAnimalName()
+
+        weightOfCat = findViewById(R.id.cat_weight)
+        setCatWeight()
+
+        bdOfCat = findViewById(R.id.cat_birthday)
+        setCatBd()
     }
 
     private fun setCatsInHouse() {
@@ -26,5 +35,16 @@ class MainActivity : AppCompatActivity() {
 
     private fun setAnimalName() {
         animalName.text = getString(R.string.animal_name, "Cat", "Tom")
+    }
+
+    private fun setCatWeight() {
+        weightOfCat.text = getString(R.string.cat_weight, 7.546534F)
+    }
+
+    private fun setCatBd() {
+        bdOfCat.text = getString(
+            R.string.cats_birthday,
+            LocalDate.of(2007, 10, 17)
+        )
     }
 }
