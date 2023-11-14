4d248e05874f751346f7f2d5c523dd877616688d
Nikolay Kochetkov <motorro@gmail.com>
2023-11-14 15:50:38 +0100

    Simple formatting

--- app/src/main/java/otus/gpb/appresources/MainActivity.kt
+++ app/src/main/java/otus/gpb/appresources/MainActivity.kt
@@ -2,10 +2,21 @@ package otus.gpb.appresources
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var catsInHouse: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        catsInHouse = findViewById(R.id.cats_in_house)
+        setCatsInHouse()
+    }
+
+    private fun setCatsInHouse() {
+        catsInHouse.text = getString(R.string.cats_in_house, 25)
     }
 }
