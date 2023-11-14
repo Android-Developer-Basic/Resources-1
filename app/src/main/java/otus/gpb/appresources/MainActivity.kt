acb4c6ba9f92ec937951719b606bf9add8785f4f
Nikolay Kochetkov <motorro@gmail.com>
2023-11-14 13:59:33 +0100

    Boilerplate

---
+++ app/src/main/java/otus/gpb/appresources/MainActivity.kt
@@ -0,0 +1,11 @@
+package otus.gpb.appresources
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
