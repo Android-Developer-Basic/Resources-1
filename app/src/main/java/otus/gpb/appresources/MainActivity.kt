050b261a153809b3a8b33b2cfa66099b1e2a56cf
Nikolay Kochetkov <motorro@gmail.com>
2023-11-14 16:18:30 +0100

    Argument position

--- app/src/main/java/otus/gpb/appresources/MainActivity.kt
+++ app/src/main/java/otus/gpb/appresources/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.TextView
 class MainActivity : AppCompatActivity() {
 
     private lateinit var catsInHouse: TextView
+    private lateinit var animalName: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,9 +15,16 @@ class MainActivity : AppCompatActivity() {
 
         catsInHouse = findViewById(R.id.cats_in_house)
         setCatsInHouse()
+
+        animalName = findViewById(R.id.animal_name)
+        setAnimalName()
     }
 
     private fun setCatsInHouse() {
         catsInHouse.text = getString(R.string.cats_in_house, 25)
+    }
+
+    private fun setAnimalName() {
+        animalName.text = getString(R.string.animal_name, "Cat", "Tom")
     }
 }
